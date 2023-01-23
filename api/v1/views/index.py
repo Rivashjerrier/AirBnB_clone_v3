a5fe5673fcbd2f44735c0450baa252ac7892e75c
Rivashjerrier <rivijerrier@gmail.com>
2023-01-23 20:04:47 +0000

    AirBnB_clone_v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """
 Flask route that returns json status response
 """
+
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -13,3 +14,14 @@ def status():
     function for status route that returns the status
     """
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ retrieves the number of each objects by type """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
